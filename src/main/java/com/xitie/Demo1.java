a49cf9cc727c23e59393ccafb9a9b691408659ea
wgy <258024044@qq.com>
2022-10-25 15:41:23 +0800

    添加了Demo1.java

---
+++ src/main/java/com/xitie/Demo1.java
@@ -0,0 +1,12 @@
+package com.xitie;
+
+/**
+ * @author wgy
+ * @version v1.0
+ * @date 2022/10/25 15:40
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+}
